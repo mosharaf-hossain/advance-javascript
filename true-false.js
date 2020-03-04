87bf0e6c47201b2e21bd3c4c445765f6f6800402
mosharaf-hossain <mosharaf868@gmail.com>
2020-03-04 15:14:35 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy
+// 0
+// ""
+//undefined
+//null
+//NuN
+//false
+//truthy
+//'0'
+
+let name = 0;
+
+
+if(name || name== 0){
+    console.log("condition is true");
+
+}
+ else{
+     console.log("condition is false")
+ }
